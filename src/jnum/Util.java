16845b4dcb4c8779e06f710f80583b7da941dd85
Attila Kovacs <attila@submm.caltech.edu>
2018-10-13 12:00:05 -0400

    0.22-1: version number updated to stable.

--- src/jnum/Util.java
+++ src/jnum/Util.java
@@ -51,9 +51,9 @@ import jnum.text.TimeFormat;
 public final class Util {
 
 
-    public final static String version = "0.22-b2";
+    public final static String version = "0.22-1";
     
-    public final static String revision = "beta";
+    public final static String revision = "";
     
     public final static String copyright = "(c)2018 Attila Kovacs"; 
     
