2e31edd950bf54bdccff7900fa7a2d7bdcd27e54
Attila Kovacs <attipaci@gmail.com>
2019-07-10 13:32:19 +0200

    0.30-1: version update to final release.

--- src/jnum/Util.java
+++ src/jnum/Util.java
@@ -52,9 +52,9 @@ import jnum.text.TimeFormat;
 public final class Util {
 
 
-    public final static String version = "0.30-a1";
+    public final static String version = "0.30-1";
     
-    public final static String revision = "devel.8";
+    public final static String revision = "";
     
     public final static String copyright = "(c)2019 Attila Kovacs"; 
     
